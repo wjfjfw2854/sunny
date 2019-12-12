eaa1bca2057b895b4b70e724af8f6a82202cb153
wjfjfw2854 <421284553@qq.com>
2019-12-12 11:28:01 +0800

    '操作傀儡数据'

--- AppStartPluginApk/appplug/src/main/java/com/example/appplug/MainActivity.kt
+++ AppStartPluginApk/appplug/src/main/java/com/example/appplug/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.appplug
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import com.example.libproxy.PluginBaseActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  *   author  : wujunfeng
@@ -13,7 +19,7 @@ import com.example.libproxy.PluginBaseActivity
  */
 class MainActivity : PluginBaseActivity() {
 
-    var x : Int ?= null
+    var x : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +28,22 @@ class MainActivity : PluginBaseActivity() {
             startActivity(intent)
         }
         x = 888
+        val curTime : Long = System.currentTimeMillis()
+        val d : Date = Date(curTime)
+        var strDate : String ?= null
+        try {
+
+            val s : SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            strDate = s.format(d)
+        }
+        catch (e : Exception)
+        {
+            e.printStackTrace()
+        }
+        val str = "x: $x" + "当前类名: " + this@MainActivity.proxyActivity!!.localClassName + " 日期: " + strDate
+        (findViewById<View>(R.id.txt1) as TextView).text = str
+        findViewById<View>(R.id.txt2).setOnClickListener {
+            (it as TextView).text = ("操作起来: " + (++x))
+        }
     }
 }
