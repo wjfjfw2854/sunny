ed621efaa0a45eebd0220250af29d4e960d2a344
wujunfeng <wujunfeng@xbmail.com.cn>
2022-12-02 14:50:09 +0800

    列表绘制完善

--- RvComplexApp/app/src/main/java/com/example/wujunfeng/recyclerviewcomplexapplication/complexrecyclerviewtool/small/TxtSmall.java
+++ RvComplexApp/app/src/main/java/com/example/wujunfeng/recyclerviewcomplexapplication/complexrecyclerviewtool/small/TxtSmall.java
@@ -3,6 +3,7 @@ package com.example.wujunfeng.recyclerviewcomplexapplication.complexrecyclerview
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.text.TextUtils;
 import com.example.wujunfeng.recyclerviewcomplexapplication.complexrecyclerviewtool.datatemple.ComplexDataTemple;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 public class TxtSmall extends Small{
     private Paint paint;
     private Object data;
+    private RectF area;
 
     public TxtSmall(Context context, SmallSpace smallSpace) {
         super(context, smallSpace);
@@ -27,7 +29,13 @@ public class TxtSmall extends Small{
         paint.setAntiAlias(true);
         paint.setTypeface(Typeface.DEFAULT_BOLD);
         paint.setTextSize(DrawTool.dp2Px(context,16));
-        paint.setColor(0xFFFF4444);
+        paint.setColor(0xFF252525);
+    }
+
+    @Override
+    public void setArea(RectF area) {
+        super.setArea(area);
+        this.area = new RectF(areaOut.left,areaOut.top,areaOut.right,areaOut.bottom);
     }
 
     @Override
@@ -93,14 +101,56 @@ public class TxtSmall extends Small{
                     val = hashMap.get(dataTempleTwo.id);
                 }
                 if(!TextUtils.isEmpty(val)) {
+                    int gap = 0;
+                    RectF rectF = new RectF(area.left,area.top,area.right,area.bottom);
+                    int colorContent = 0xff252525;//默认白版字体颜色
                     int gravity = DrawTool.CENTER;
-                    if (space.args != null && space.args.length > 0) {
-                        Object obj = space.args[0];
-                        if (obj instanceof Integer) {
-                            gravity = ((Integer)obj).intValue();
+                    Object[] args = space.args;
+                    if (args != null) {
+                        if (args.length > 0) {
+                            Object obj = args[0];
+                            if (obj instanceof Integer) {
+                                gravity = ((Integer) obj).intValue();
+                            }
+                        }
+                        if (args.length > 4) {
+                            Object obj = args[4];
+                            if (obj != null && obj instanceof int[][]) {
+                                int[][] bgs = (int[][]) obj;
+                                int len = bgs.length;
+                                float valF = .1f;
+                                try {
+                                    valF = Float.parseFloat(val);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                if (len > 1) {
+                                    int[] bg = bgs[0];
+                                    if (bg != null && bg.length > 2) {
+                                        if (valF > 0) {
+                                            paint.setColor(bg[0]);
+                                        } else if (valF < 0) {
+                                            paint.setColor(bg[1]);
+                                        } else {
+                                            paint.setColor(bg[2]);
+                                        }
+                                    }
+                                    int[] gapOut = bgs[1];
+                                    if (gapOut != null && gapOut.length > 0) {
+                                        gap = gapOut[0];
+                                    }
+                                }
+                                if (len > 2) {
+                                    int[] colorFont = bgs[2];
+                                    colorContent = colorFont[0];//白版颜色 colorFont[1]-黑版字体颜色
+                                }
+                                rectF = new RectF(area.left+gap,area.top+gap,area.right-gap,area.bottom-gap);
+                                DrawTool.drawBg(canvas,paint,rectF);
+                            }
                         }
                     }
-                    DrawTool.drawRectText(canvas, val, paint, areaSubstance, gravity, true);
+                    paint.setColor(colorContent);
+                    DrawTool.drawRectText(canvas, val, paint, rectF, gravity, true);
                 }
             }
         }
